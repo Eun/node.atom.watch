93cb28e895c496ace2335d8bf5da1603635a0764
Eun <eun@su.am>
2016-12-19 16:10:16 +0100

    Always UTC time

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 function handleRequest(request, response){
 response.setHeader('Access-Control-Allow-Origin', '*');
-response.end('{"TimeStamp": ' + Date.now() + '}');
+response.end('{"TimeStamp": ' + new Date(Date.now()+(new Date().getTimezoneOffset()*60000)).getTime() + '}');
 }
 var server = http.createServer(handleRequest);
 server.listen(process.env.PORT || 5000);
