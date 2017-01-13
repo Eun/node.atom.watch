3916b857314e49c1870837b2a21a60f6f669d34c
Tobias Salzmann <eun@su.am>
2017-01-13 11:41:22 +0100

    set json header

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 function handleRequest(request, response){
 response.setHeader('Access-Control-Allow-Origin', '*');
+response.setHeader('Content-Type', 'application/json');
 response.end('{"TimeStamp": ' + new Date(Date.now()+(new Date().getTimezoneOffset()*60000)).getTime() + '}');
 }
 var server = http.createServer(handleRequest);
